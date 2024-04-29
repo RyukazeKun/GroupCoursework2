d299383671eac95d46ea58124fe118dbad6ae10c
Jtyrwhitt <98902009+Jtyrwhitt@users.noreply.github.com>
2024-04-29 19:19:56 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,28 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Customer(models.Model):                           # customer model
-    customerID = models.AutoField(primary_key = True)
-    customerUsername = models.CharField(max_length = 100, unique = True)
-    customerPassword = models.CharField(max_length = 100)
-    customerFirstName = models.CharField(max_length = 100)
-    customerLastName = models.CharField(max_length = 100)
-    customer = models.ForeignKey(Order, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return f'{self.customerFirstName} {self.customerLastName}'
-
-class Order(models.Model):                              # Order model
-    orderID = models.AutoField(primary_key = True)
-    orderDate = models.DateField()
-    orderReturnDate = models.DateField()
-    orderDeviceName = models.CharField(max_length = 100)
-    orderQuantity = models.PositiveIntegerField()
-    orderStatus = models.CharField(max_length = 50)
-    order = models.ForeignKey(Admin, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f'{self.orderID} : {self.orderDeviceName} ({self.orderStatus})'
 
 class Admin(models.Model):                              # Admin model
     adminID = models.AutoField(primary_key = True)
@@ -34,21 +12,28 @@ class Admin(models.Model):                              # Admin model
     def __str__(self):
         return f'{self.adminFirstName} {self.adminLastName}'
 
-class Equipment(models.Model):                          # Equipment model
-    equipmentID = models.AutoField(primary_key = True)
-    equipmentName = models.CharField(max_length = 100)
-    equipmentType = models.CharField(max_length = 100)
-    quantity = models.PositiveIntegerField()
-    audit = models.CharField(max_length = 100)
-    status = models.CharField(max_length = 100)
-    bookingHistory = models.CharField(max_length = 100)
-    location = models.CharField(max_length = 100)
-    admin = models.ForeignKey(Admin, on_delete = models.CASCADE)
-    order = models.ForeignKey(Order, on_delete = models.CASCADE)
-    deviceSerialNo = models.ForeignKey(Device, on_delete = models.CASCADE)
-    
+class Order(models.Model):                              # Order model
+    orderID = models.AutoField(primary_key = True)
+    orderDate = models.DateField()
+    orderReturnDate = models.DateField()
+    orderDeviceName = models.CharField(max_length = 100)
+    orderQuantity = models.PositiveIntegerField()
+    orderStatus = models.CharField(max_length = 50)
+    order = models.ForeignKey(Admin, on_delete = models.CASCADE, null=True)
+
     def __str__(self):
-        return f'{self.equipmentName} ({self.equipmentType})'
+        return f'{self.orderID} : {self.orderDeviceName} ({self.orderStatus})'
+
+class Customer(models.Model):                           # customer model
+    customerID = models.AutoField(primary_key = True)
+    customerUsername = models.CharField(max_length = 100, unique = True)
+    customerPassword = models.CharField(max_length = 100)
+    customerFirstName = models.CharField(max_length = 100)
+    customerLastName = models.CharField(max_length = 100)
+    customer = models.ForeignKey(Order, on_delete = models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.customerFirstName} {self.customerLastName}'
 
 class Device(models.Model):                         # Device model
     deviceSerialNo = models.AutoField(primary_key = True)
@@ -60,3 +45,22 @@ class Device(models.Model):                         # Device model
 
     def __str__(self):
         return self.deviceName
+
+class Equipment(models.Model):                          # Equipment model
+    equipmentID = models.AutoField(primary_key = True)
+    equipmentName = models.CharField(max_length = 100)
+    equipmentType = models.CharField(max_length = 100)
+    quantity = models.PositiveIntegerField()
+    audit = models.DateField(null=True)
+    status = models.CharField(max_length = 100)
+    bookingHistory = models.CharField(max_length = 100, null=True)
+    location = models.CharField(max_length = 100)
+    status = models.CharField(max_length = 50, null=True)
+    admin = models.ForeignKey(Admin, on_delete = models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete = models.CASCADE, null=True)
+    deviceSerialNo = models.ForeignKey(Device, on_delete = models.CASCADE, null=True)
+    
+    def __str__(self):
+        return f'{self.equipmentName} ({self.equipmentType})'
+
+

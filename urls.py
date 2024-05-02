49a99deae5c8b1351bdc5e25480fcb8597a73c0f
Royal Rai <123957992+RyukazeKun@users.noreply.github.com>
2024-05-02 03:55:01 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('studentapp.urls')),
+    path('studentapp/', include(('studentapp.urls', 'studentapp'), namespace='studentapp')),
+
 ]
